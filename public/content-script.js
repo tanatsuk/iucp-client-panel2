87acbcf33a3e2d145c553dee9d0d1cfffda19652
tatsuo <tatsuonatsukawa@gmail.com>
2024-03-08 05:33:09 +0900

    a cosmetic change

--- public/content-script.js
+++ public/content-script.js
@@ -1,2 +1,2 @@
-$("#main").prepend("<div id=iucp-modal class=ui-widget-content><p>hola</p></div>");
+$("#main").prepend("<div id=iucp-modal class=ui-widget-content><p>IUCP Client Panel</p><p>You can drag me around</p></div>");
 $( function() {$( "#iucp-modal" ).draggable();});
