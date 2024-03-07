c47bc3e0807b679e8c5cf31b26d5a2c58c8c6eb2
tatsuo <tatsuonatsukawa@gmail.com>
2024-03-08 01:33:54 +0900

    no longer creates a gaping gap at the top

--- public/content-script.js
+++ public/content-script.js
@@ -1,2 +1,2 @@
-$("body").prepend("<div id=iucp-modal class=ui-widget-content><p>hola</p></div>");
+$("#main").prepend("<div id=iucp-modal class=ui-widget-content><p>hola</p></div>");
 $( function() {$( "#iucp-modal" ).draggable();});
